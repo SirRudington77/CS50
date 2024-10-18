9f1eb583a2565c010f3817e9b8aa09abc6c80300
SirRudington77 <sirrudington@outlook.com>
2024-10-18 12:36:57 -0400

    Completed final assignment

--- CS50/Week1/MaelTime.py
+++ CS50/Week1/MaelTime.py
@@ -3,3 +3,30 @@ In meal.py, implement a program that prompts the user for a time and outputs whe
 
 Structure your program per the below, wherein convert is a function (that can be called by main) that converts time, a str in 24-hour format, to the corresponding number of hours as a float. For instance, given a time like "7:30" (i.e., 7 hours and 30 minutes), convert should return 7.5 (i.e., 7.5 hours).
 """
+# create a convert function that is called by main
+def main():
+    # Prompt user for a time. 24-hour format #:## or ##:##
+    x = input("What time is it?: ")
+
+    # Output either breakfast, lunch or dinner. 
+    if 6.0 <= convert(x) <= 10.0:
+        print ("breakfast time")
+    elif 12.0 <= convert(x) <= 14.0:
+        print("lunch time")
+    elif 18.0 <= convert(x) <= 21.0:
+        print("dinner time")
+    # if not a meal, no output
+    else:
+        pass
+
+def convert(x):
+    hour, minute = x.split(":")
+    # converts string to a float. 7:30 or 07:30 converts and returns 7.5
+    hour = float(hour)
+    minute = float(minute)
+    decimalConversion = float(hour + (minute / 60))
+    return round(decimalConversion)
+
+
+if __name__ == "__main__":
+    main()
